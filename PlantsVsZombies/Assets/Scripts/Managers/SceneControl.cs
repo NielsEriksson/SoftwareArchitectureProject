ffae4ae1a3ffe2463eaa2d0de00e3f60245e1371
SaraGolob <saragolob@hotmail.com>
2023-05-18 21:58:37 +0200

    menu
    menu

---
+++ PlantsVsZombies/Assets/Scripts/Managers/SceneControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    int currentScene = 0;
+    // Start is called before the first frame update
+    public void LoadScene(int scene)
+    {
+        //Debug.Log("loading scene with the number " + scene);
+        SceneManager.LoadScene(scene);
+    }
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        //Debug.Log("loading scene with the name " + scene);
+    }
+    public void LoadNextScene()
+    {
+        //Debug.Log("loading next scene");
+        currentScene++;
+        SceneManager.LoadScene(currentScene);
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(currentScene);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

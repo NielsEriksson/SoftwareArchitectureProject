ed7fdcb89f7397e4c7fb5cb75433f8da94aab3b9
SaraGolob <89583211+SaraGolob@users.noreply.github.com>
2023-05-23 11:11:58 +0200

    Element Hud

--- PlantsVsZombies/Assets/Scripts/ElementControl.cs
+++ PlantsVsZombies/Assets/Scripts/ElementControl.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class ElementControl : MonoBehaviour
 {
     [SerializeField] public List<Plant.Element> elements = new List<Plant.Element>();
+    [SerializeField] private TMP_Text Water;
+    [SerializeField] private TMP_Text Sun;
+    [SerializeField] private TMP_Text Poison;
+    int WaterNumber;
+    int SunNumber;
+    int PoisonNumber;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +22,9 @@ public class ElementControl : MonoBehaviour
     public void UpdateElements()
     {
         elements.Clear();
-        
+        WaterNumber = 0;
+        SunNumber = 0;
+        PoisonNumber = 0;
         GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Plant");
         List<Plant> plants = new List<Plant>();
 
@@ -29,7 +38,19 @@ public class ElementControl : MonoBehaviour
             foreach (Plant.Element element in temp)
             {
                 elements.Add(element);
+                if (element == Plant.Element.Light){
+                    SunNumber++;
+                }
+                else if (element == Plant.Element.Water){
+                    WaterNumber++;
+                }
+                else if (element == Plant.Element.Poison){
+                    PoisonNumber++;
+                }
             }
         }
+        Water.text = WaterNumber.ToString();
+        Sun.text = SunNumber.ToString();
+        Poison.text = PoisonNumber.ToString();
     }
 }

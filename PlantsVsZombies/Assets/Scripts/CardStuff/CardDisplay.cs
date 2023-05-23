141033e672eabe8f4c4eb59297a11f8a463e40fb
VexMagic <89905295+VexMagic@users.noreply.github.com>
2023-05-23 23:33:40 +0200

    Added so element conditions appear on cards

--- PlantsVsZombies/Assets/Scripts/CardStuff/CardDisplay.cs
+++ PlantsVsZombies/Assets/Scripts/CardStuff/CardDisplay.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,9 +13,13 @@ public class CardDisplay : MonoBehaviour
     public RawImage art;
     public TMP_Text descriptionText;
     public TMP_Text extraDescriptionText;
+    public GameObject baseElement;
 
     Plant.Element[] conditions;
     public Texture2D[] elementIcons = new Texture2D[Enum.GetNames(typeof(Plant.Element)).Length];
+    List<GameObject> elementObject = new List<GameObject>();
+
+    const float elementDistance = 20f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +32,15 @@ public class CardDisplay : MonoBehaviour
             conditions = card.conditions;
             extraDescriptionText.text = card.extraDescription;
 
-            //public string cardName;
-            //public Image cardImage;
-            //public string cardDescription;
-            //public Plant.Element[] conditions;
-            //public string extraDescription;
+            int counter = 0;
+            foreach (var element in conditions)
+            {
+                GameObject tempObject = Instantiate(baseElement, GetComponentInChildren<HorizontalLayoutGroup>().gameObject.transform);
+                tempObject.GetComponent<RawImage>().texture = elementIcons[(int)element];
+                //float tempX = ((float)(conditions.Length - 1) / 2) - counter;
+                //tempObject.transform.position += new Vector3(tempX * elementDistance, 0, 0);
+                elementObject.Add(tempObject);
+            }
         }
     }
 
